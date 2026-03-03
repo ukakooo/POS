77091054b8d1ab2d2ad72b9ebd591c4316c11616
Harutoo <herconaryangga15@gmail.com>
2026-03-03 17:13:44 +0700

    DB Facade

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -8,8 +8,14 @@ use Illuminate\Support\Facades\DB;
 class LevelController extends Controller
 {
     public function index() {
-        DB::insert('insert into m_level (level_kode, level_nama, created_at) values (?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+        // DB::insert('insert into m_level (level_kode, level_nama, created_at) values (?, ?, ?)', ['CUS', 'Pelanggan', now()]);
 
-        return 'Insert data baru berhasil.';
+        // return 'Insert data baru berhasil.';
+
+        // $row = DB::update('update m_level SET level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
+        // return 'Update data berhasil. Jumlah data yang diupdate: ' .$row.' baris';
+
+        $data = DB::select('select * from m_level');
+        return view('level', ['data' => $data]);
     }
 }
