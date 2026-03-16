e7bc37a0addc60a674246da6c26cc37b68b64ef2
Harutoo <herconaryangga15@gmail.com>
2026-03-16 12:41:36 +0700

    Controller syntaxes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,15 +28,23 @@ class UserController extends Controller
         // $user = UserModel::all();
         // return view('user', ['data' => $user]);
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
+        // $user = UserModel::all();
+
+        // $user = UserModel::find(1);
+        
+        // $user = UserModel::where('level_id', 1)->firstWhere('level_id', 1); 
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
