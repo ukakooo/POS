33c4ca38d3304bca33f467997222e0e2ea493474
Harutoo <herconaryangga15@gmail.com>
2026-03-16 13:00:17 +0700

    ThrowsExceptionHandler

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,9 +42,13 @@ class UserController extends Controller
         
         // $user = UserModel::where('level_id', 1)->firstWhere('level_id', 1); 
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+
+        // $user = UserModel::findOrFail(1);
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
